abc4d94b8c5f3c5dc1e20c4de58a74fa3ba3ef41
Yahir <tyahir27@gmail.com>
2024-04-09 19:19:29 -0500

    barcode sacnner created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from "@angular/core";
+import { BarcodeScannerLivestreamComponent } from "ngx-barcode-scanner";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'productsCounter';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild(BarcodeScannerLivestreamComponent)
+  public barcodeScanner!: BarcodeScannerLivestreamComponent;
+
+  barcodeValue: any;
+
+  ngAfterViewInit() {
+    this.barcodeScanner.start();
+  }
+
+  onValueChanges(result: any) {
+    this.barcodeValue = result.codeResult.code;
+  }
+
+  onStarted(started: any) {
+    console.log(started);
+  }
 }
